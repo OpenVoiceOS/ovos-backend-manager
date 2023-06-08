cbe408b5632ab923bd6cb942e67ea08c418e258c
JarbasAi <jarbasai@mailfence.com>
2023-06-09 00:00:23 +0100

    feat/admin_get_backend_config

--- ovos_backend_manager/backend.py
+++ ovos_backend_manager/backend.py
@@ -5,39 +5,38 @@ from pywebio.input import textarea, select, actions
 from pywebio.output import put_table, put_markdown, popup, put_code, put_image, use_scope
 from ovos_backend_manager.apis import ADMIN, GEO
 
-# TODO - from backend
-CONFIGURATION = {}
-
 
 def backend_menu(back_handler=None):
+    backend_config = ADMIN.get_backend_config()
+
     with use_scope("logo", clear=True):
         img = open(f'{os.path.dirname(__file__)}/res/backend_config.png', 'rb').read()
         put_image(img)
 
     with use_scope("main_view", clear=True):
         put_table([
-            ['Backend Port', CONFIGURATION["server"]["port"]],
-            ['Device Authentication enabled', not CONFIGURATION["server"]["skip_auth"]],
-            ['Location override enabled', CONFIGURATION["server"]["override_location"]],
-            ['IP Geolocation enabled', CONFIGURATION["server"]["geolocate"]],
-            ['Default TTS', CONFIGURATION["tts"]["module"]],
-            ['Default Wake Word', CONFIGURATION["listener"]["wake_word"]],
-            ['Default date format', CONFIGURATION["date_format"]],
-            ['Default time format', CONFIGURATION["time_format"]],
-            ['Default system units', CONFIGURATION["system_unit"]]
+            ['Backend Port', backend_config["server"]["port"]],
+            ['Device Authentication enabled', not backend_config["server"]["skip_auth"]],
+            ['Location override enabled', backend_config["server"]["override_location"]],
+            ['IP Geolocation enabled', backend_config["server"]["geolocate"]],
+            ['Default TTS', backend_config["tts"]["module"]],
+            ['Default Wake Word', backend_config["listener"]["wake_word"]],
+            ['Default date format', backend_config["date_format"]],
+            ['Default time format', backend_config["time_format"]],
+            ['Default system units', backend_config["system_unit"]]
         ])
         put_markdown(f'### Default location:')
         put_table([
-            ['Timezone Code', CONFIGURATION["location"]["timezone"]["code"]],
-            ['City', CONFIGURATION["location"]["city"]["name"]],
-            ['State', CONFIGURATION["location"]["city"]["state"]["name"]],
-            ['Country', CONFIGURATION["location"]["city"]["state"]["country"]["name"]],
-            ['Country Code', CONFIGURATION["location"]["city"]["state"]["country"]["code"]],
-            ['Latitude', CONFIGURATION["location"]["coordinate"]["latitude"]],
-            ['Longitude', CONFIGURATION["location"]["coordinate"]["longitude"]]
+            ['Timezone Code', backend_config["location"]["timezone"]["code"]],
+            ['City', backend_config["location"]["city"]["name"]],
+            ['State', backend_config["location"]["city"]["state"]["name"]],
+            ['Country', backend_config["location"]["city"]["state"]["country"]["name"]],
+            ['Country Code', backend_config["location"]["city"]["state"]["country"]["code"]],
+            ['Latitude', backend_config["location"]["coordinate"]["latitude"]],
+            ['Longitude', backend_config["location"]["coordinate"]["longitude"]]
         ])
 
-    auth = 'Enable device auth' if not CONFIGURATION["server"]["skip_auth"] else 'Disable device auth'
+    auth = 'Enable device auth' if not backend_config["server"]["skip_auth"] else 'Disable device auth'
 
     buttons = [{'label': auth, 'value': "auth"},
                {'label': 'Set default location', 'value': "geo"},
@@ -51,7 +50,7 @@ def backend_menu(back_handler=None):
     if back_handler:
         buttons.insert(0, {'label': '<- Go Back', 'value': "main"})
 
-    if CONFIGURATION["server"]["override_location"]:
+    if backend_config["server"]["override_location"]:
         buttons.insert(-2, {'label': 'Enable IP geolocation', 'value': "ip_geo"})
     else:
         buttons.insert(-2, {'label': 'Enable location override', 'value': "loc_override"})
@@ -64,59 +63,59 @@ def backend_menu(back_handler=None):
         return
     elif opt == "tts":
         # TODO - opm scan
-        tts = select("Choose a voice", list(k for k in CONFIGURATION["tts"].keys()
+        tts = select("Choose a voice", list(k for k in backend_config["tts"].keys()
                                             if k not in ["module"]))
-        CONFIGURATION["tts"]["module"] = tts
+        backend_config["tts"]["module"] = tts
         with popup(f"Default TTS set to: {tts}"):
-            put_code(json.dumps(CONFIGURATION["tts"][tts], ensure_ascii=True, indent=2), "json")
+            put_code(json.dumps(backend_config["tts"][tts], ensure_ascii=True, indent=2), "json")
     elif opt == "ww":
         # TODO - opm scan
         ww = select("Choose a wake word",
-                    list(CONFIGURATION["hotwords"].keys()))
-        CONFIGURATION["listener"]["wake_word"] = ww
+                    list(backend_config["hotwords"].keys()))
+        backend_config["listener"]["wake_word"] = ww
         with popup(f"Default wake word set to: {ww}"):
-            put_code(json.dumps(CONFIGURATION["ww_configs"][ww], ensure_ascii=True, indent=2), "json")
+            put_code(json.dumps(backend_config["ww_configs"][ww], ensure_ascii=True, indent=2), "json")
     elif opt == "geo":
         loc = textarea("Enter an address",
                        placeholder="Anywhere street Any city Nº234",
                        required=True)
         data = GEO.get_geolocation(loc)
-        CONFIGURATION["location"] = data
+        backend_config["location"] = data
         with popup(f"Default location set to: {loc}"):
             put_code(json.dumps(data, ensure_ascii=True, indent=2), "json")
     elif opt == "loc_override":
-        CONFIGURATION["server"]["override_location"] = True
-        CONFIGURATION["server"]["geolocate"] = False
+        backend_config["server"]["override_location"] = True
+        backend_config["server"]["geolocate"] = False
         popup("Location override enabled!")
     elif opt == "ip_geo":
-        CONFIGURATION["server"]["geolocate"] = True
-        CONFIGURATION["server"]["override_location"] = False
+        backend_config["server"]["geolocate"] = True
+        backend_config["server"]["override_location"] = False
         popup("IP Geolocation enabled!")
     elif opt == "auth":
-        CONFIGURATION["server"]["skip_auth"] = not CONFIGURATION["server"]["skip_auth"]
-        if CONFIGURATION["server"]["skip_auth"]:
+        backend_config["server"]["skip_auth"] = not backend_config["server"]["skip_auth"]
+        if backend_config["server"]["skip_auth"]:
             popup("Device authentication enabled!")
         else:
             popup("Device authentication disabled! Pairing will not be needed")
     elif opt == "date":
-        CONFIGURATION["date_format"] = select("Change date format",
+        backend_config["date_format"] = select("Change date format",
                                               ['DMY', 'MDY'])
-        popup(f"Default date format set to: {CONFIGURATION['date_format']}")
+        popup(f"Default date format set to: {backend_config['date_format']}")
     elif opt == "time":
-        CONFIGURATION["time_format"] = select("Change time format",
+        backend_config["time_format"] = select("Change time format",
                                               ['full', 'short'])
-        popup(f"Default time format set to: {CONFIGURATION['time_format']}")
+        popup(f"Default time format set to: {backend_config['time_format']}")
     elif opt == "unit":
-        CONFIGURATION["system_unit"] = select("Change system units",
+        backend_config["system_unit"] = select("Change system units",
                                               ['metric', 'imperial'])
-        popup(f"Default system units set to: {CONFIGURATION['system_unit']}")
+        popup(f"Default system units set to: {backend_config['system_unit']}")
     elif opt == "email":
         email = textarea("Enter default notifications email",
                          placeholder="notify@me.com",
                          required=True)
-        CONFIGURATION["email"]["recipient"] = email
+        backend_config["email"]["recipient"] = email
 
     if opt != "view":
-        ADMIN.update_backend_config(CONFIGURATION)
+        ADMIN.update_backend_config(backend_config)
 
     backend_menu(back_handler=back_handler)
--- ovos_backend_manager/microservices.py
+++ ovos_backend_manager/microservices.py
@@ -40,12 +40,11 @@ def microservices_menu(back_handler=None):
         img = open(f'{os.path.dirname(__file__)}/res/microservices_config.png', 'rb').read()
         put_image(img)
 
-    # TODO - config from admin api
-    CONFIGURATION = {}
+    backend_config = ADMIN.get_backend_config()
 
     with use_scope("main_view", clear=True):
         put_table([
-            ['STT module', CONFIGURATION["stt"]["module"]]
+            ['STT module', backend_config["stt"]["module"]]
         ])
 
     buttons = [{'label': 'Configure STT', 'value': "stt"},
@@ -70,44 +69,44 @@ def microservices_menu(back_handler=None):
         stt = select("Choose a speech to text engine", opts)
         cfg = dict(cfgs[stt])
         m = cfg.pop("module")
-        CONFIGURATION["stt"]["module"] = m
-        CONFIGURATION["stt"][m] = cfg
+        backend_config["stt"]["module"] = m
+        backend_config["stt"][m] = cfg
         with popup(f"STT set to: {stt}"):
             put_code(json.dumps(cfg, ensure_ascii=True, indent=2), "json")
     elif opt == "secrets":
         data = input_group('Secret Keys', [
-            input("WolframAlpha key", value=CONFIGURATION["microservices"]["wolfram_key"],
+            input("WolframAlpha key", value=backend_config["microservices"]["wolfram_key"],
                   type=TEXT, name='wolfram'),
-            input("OpenWeatherMap key", value=CONFIGURATION["microservices"]["owm_key"],
+            input("OpenWeatherMap key", value=backend_config["microservices"]["owm_key"],
                   type=TEXT, name='owm')
         ])
-        CONFIGURATION["microservices"]["wolfram_key"] = data["wolfram"]
-        CONFIGURATION["microservices"]["owm_key"] = data["owm"]
+        backend_config["microservices"]["wolfram_key"] = data["wolfram"]
+        backend_config["microservices"]["owm_key"] = data["owm"]
         popup("Secrets updated!")
     elif opt == "smtp":
         # TODO  - ovos / neon endpoint
 
-        if "smtp" not in CONFIGURATION["email"]:
-            CONFIGURATION["email"]["smtp"] = {}
+        if "smtp" not in backend_config["email"]:
+            backend_config["email"]["smtp"] = {}
 
         data = input_group('SMTP Configuration', [
-            input("Username", value=CONFIGURATION["email"]["smtp"].get("username", 'user'),
+            input("Username", value=backend_config["email"]["smtp"].get("username", 'user'),
                   type=TEXT, name='username'),
-            input("Password", value=CONFIGURATION["email"]["smtp"].get("password", '***********'),
+            input("Password", value=backend_config["email"]["smtp"].get("password", '***********'),
                   type=TEXT, name='password'),
-            input("Host", value=CONFIGURATION["email"]["smtp"].get("host", 'smtp.mailprovider.com'),
+            input("Host", value=backend_config["email"]["smtp"].get("host", 'smtp.mailprovider.com'),
                   type=TEXT, name='host'),
-            input("Port", value=CONFIGURATION["email"]["smtp"].get("port", '465'),
+            input("Port", value=backend_config["email"]["smtp"].get("port", '465'),
                   type=NUMBER, name='port')
         ])
 
-        CONFIGURATION["email"]["smtp"]["username"] = data["username"]
-        CONFIGURATION["email"]["smtp"]["password"] = data["password"]
-        CONFIGURATION["email"]["smtp"]["host"] = data["host"]
-        CONFIGURATION["email"]["smtp"]["port"] = data["port"]
+        backend_config["email"]["smtp"]["username"] = data["username"]
+        backend_config["email"]["smtp"]["password"] = data["password"]
+        backend_config["email"]["smtp"]["host"] = data["host"]
+        backend_config["email"]["smtp"]["port"] = data["port"]
         with popup(f"SMTP configuration for: {data['host']}"):
             put_code(json.dumps(data, ensure_ascii=True, indent=2), "json")
 
-    ADMIN.update_backend_config(CONFIGURATION)
+    ADMIN.update_backend_config(backend_config)
 
     microservices_menu(back_handler=back_handler)
